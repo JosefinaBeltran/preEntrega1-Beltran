e26c5ebd183e7ac54dadee4a53a1879a5f390e93
Josefina <josefinabeltran93@gmail.com>
2024-09-07 21:19:42 -0300

    Entrega 1 final

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,6 +1,12 @@
-function ItemListContainer(){
+import "./ItemListContainer.css"
+import { GiSpellBook } from "react-icons/gi"
+
+function ItemListContainer({greeting}){
     return(
-        <div></div>
+        <div className="bienvenida">
+            <h1>{greeting}</h1>
+            <GiSpellBook size="100px" color="#fdf9f2"/>
+        </div>
     )
 }
 
